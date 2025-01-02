6584b3e3e6997129ae97323e5316071363e1ca3b
tarakichi <tarakichi48@gmail.com>
2025-01-03 01:37:17 +0900

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -8,7 +8,7 @@ function last() {
     if (result.isConfirmed && result.value == "景雲飛") {
       Swal.fire({
         title: "よくやった",
-        html: "三文字目は<br><strong style='color: white; font-size: 3rem;'>飛</strong>",
+        html: "",
       }).then((result) => {
         if (result.isConfirmed) {
           window.location.href = "clear.html";
